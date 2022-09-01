e13de4d6d633131e2f65aea5bfe600f79570c93b
TheFred94 <frederikrlarsen@gmail.com>
2022-09-01 09:28:05 +0200

    kort beskrivelse + pris hentes ind

--- Babushka/script.js
+++ Babushka/script.js
@@ -24,11 +24,14 @@ function visRetter() {
   const template = document.querySelector("template").content;
   //   Sletter indhold fra main ved skift af filter
   main.textContent = "";
+  //   Loop gennem json
   retter.forEach((ret) => {
     if (filter == ret.drikkevarer || filter == "alle") {
       const klon = template.cloneNode(true);
       klon.querySelector(".billede").src = "billeder/" + ret.billednavn + "-md.jpg";
       klon.querySelector(".ret").textContent = ret.navn;
+      klon.querySelector(".kort_beskrivelse").textContent = ret.kortbeskrivelse;
+      klon.querySelector(".pris").textContent = ret.pris + " kr.";
       main.appendChild(klon);
     }
   });
